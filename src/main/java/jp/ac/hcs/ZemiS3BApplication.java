7f0bec260e1ecaf902b5df4547ed1c566b8351e2
s20193057 <s20193057@HCS54M109.stdlan.hcs.local>
2021-10-28 15:41:58 +0900

    初回

---
+++ src/main/java/jp/ac/hcs/ZemiS3BApplication.java
@@ -0,0 +1,13 @@
+package jp.ac.hcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZemiS3BApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZemiS3BApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/ac/hcs/ZemiS3BApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.ac.hcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZemiS3BApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
