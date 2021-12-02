b3b82c32e856fc55d6d7ba8f7912bda2d3ecfcaa
s20193057 <s20193057@HCS54M109.stdlan.hcs.local>
2021-12-02 09:51:22 +0900

    くぁｗせｄｒｆｔｇｙふじこｌｐ；

---
+++ src/main/java/jp/ac/hcs/JobManagmentKsApplication.java
@@ -0,0 +1,13 @@
+package jp.ac.hcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobManagmentKsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobManagmentKsApplication.class, args);
+	}
+
+}
