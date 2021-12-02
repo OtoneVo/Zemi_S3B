f38d9a2a6b289659fbc828f844cbda7d70a86f4c
s20193023 <s20193023@HCS54M118.stdlan.hcs.local>
2021-12-02 11:17:35 +0900

    各テーブルのデータ追加

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -10,3 +10,27 @@ VALUES('kashiwaya@hcs.ac.jp', '$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2ASFjDhe6WF
 /* ユーザマスタのデータ(病院権限) PASS:password */
 INSERT INTO m_user (user_id, encrypted_password, user_name, user_permission, gender, age, birth_date, address, phone_number)
 VALUES('ima@hcs.ac.jp', '$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2ASFjDhe6WF4.Qji1huIPa', '井馬渉太', 'HOSPITAL', 1, 21, '2000-07-29', '札幌市東区北4条東6丁目3-50', '090-7243-5143');
+
+
+/* 病院マスタのデータ PASS:password */
+INSERT INTO hospital_list (hospital_id, hospital_name, encrypted_password, address, phone_number, number_of_reservations)
+VALUES('sapporo@hosp.ac.jp', '札幌市立病院', '$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2ASFjDhe6WF4.Qji1huIPa', '札幌市中央区北10条東5丁目', '090-2435-8204' , '30');
+
+
+/* 予約マスタテーブル */
+INSERT INTO hospital_list (hospital_id, user_id, diagnosis_id, reservation_date, reservation_time)
+VALUES('sapporo@hosp.ac.jp', 'kashiwaya@hcs.ac.jp', '1', '2021-12-22', '12:30:00');
+
+
+/* 診療科マスタテーブル 1：外科 */
+INSERT INTO hospital_list (diagnosis_id, diagnosis_name)
+VALUES('1', '外科');
+
+/* 診療科マスタテーブル 2：内科 */
+INSERT INTO hospital_list (diagnosis_id, diagnosis_name)
+VALUES('2', '内科');
+
+
+/* 病院診療科テーブル */
+INSERT INTO hospital_list (hospital_id, diagnosis_id)
+VALUES('sapporo@hosp.ac.jp', '1');
