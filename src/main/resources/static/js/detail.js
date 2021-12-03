e7aec743c6a6ce148add1a26eb69712625d07633
s20193052 <s20193052@HCS54M114.stdlan.hcs.local>
2021-12-03 12:22:21 +0900

    トップ画面のハンバーガーメニューを作りました

--- src/main/resources/static/js/detail.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-  $('.js-open').click(function () {
-    $('#overlay, .modal-window').fadeIn();
-  });
-  $('.js-close').click(function () {
-    $('#overlay, .modal-window').fadeOut();
-  });
-});
