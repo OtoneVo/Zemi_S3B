629b8e94e28e291eba060da644895df5408a69c7
s20193052 <s20193052@HCS54M114.stdlan.hcs.local>
2022-01-25 09:20:33 +0900

    新規登録画面に遷移成功

--- src/main/java/jp/ac/hcs/user/UserController.java
+++ src/main/java/jp/ac/hcs/user/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -79,6 +80,8 @@ public class UserController {
 	public String management(Principal principal, Model model) {
 
 		try {
+			UserData userData = userService.userUpdatetRansition(principal.getName());
+			model.addAttribute("userData", userData);
 			log.info(principal.getName() + "：ユーザ管理画面：正常");
 		} catch (DataAccessException e) {
 			log.info(principal.getName() + "：ユーザ管理画面：異常");
@@ -97,6 +100,28 @@ public class UserController {
 	 */
 	@PostMapping("/userList/update")
 	public String userUpdatetRansition(@RequestParam("user_id") String user_id, Principal principal, Model model) {
+
+		try {
+			UserData userData = userService.userUpdatetRansition(user_id);
+			model.addAttribute("userData", userData);
+			log.info(principal.getName() + "：ユーザ情報更新画面：正常");
+		} catch (DataAccessException e) {
+			log.info(principal.getName() + "：ユーザ情報更新画面：異常");
+			return "errorMessage";
+		}
+		return "user/userUpdate";
+
+	}
+
+	/**
+	 * ユーザ情報更新画面に遷移する
+	 *
+	 * @param	principal		ログイン中のユーザ情報
+	 * @param	model			モデル情報
+	 * @return	userUpdate	正常：ユーザ情報更新画面 errorMessage 異常：エラーメッセージ表示画面
+	 */
+	@GetMapping("/userList/update/{user_id}")
+	public String userUpdateOne(@PathVariable("user_id") String user_id, Principal principal, Model model) {
 
 		try {
 			UserData userData = userService.userUpdatetRansition(user_id);
@@ -204,6 +229,26 @@ public class UserController {
 		}
 
 		return "user/userUpdate";
+
+	}
+
+	/**
+	 * ユーザ情報更新画面に遷移する
+	 *
+	 * @param	principal		ログイン中のユーザ情報
+	 * @param	model			モデル情報
+	 * @return	userUpdate	正常：ユーザ情報更新画面 errorMessage 異常：エラーメッセージ表示画面
+	 */
+	@GetMapping("/userList/userInsert")
+	public String userInsertOne(String user_id, Principal principal, Model model) {
+
+		try {
+			log.info(principal.getName() + "：ユーザ情報更新画面：正常");
+		} catch (DataAccessException e) {
+			log.info(principal.getName() + "：ユーザ情報更新画面：異常");
+			return "errorMessage";
+		}
+		return "user/userInsert";
 
 	}
 
