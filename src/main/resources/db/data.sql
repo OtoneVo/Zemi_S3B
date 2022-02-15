05a0c558f69b1da8e1ef16da1830ddde638993ec
s20193057 <s20193057@HCS54M109.stdlan.hcs.local>
2022-02-15 09:49:22 +0900

    adminですー

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -3,7 +3,7 @@
 
 /* ユーザマスタのデータ（ADMIN権限） PASS:pasword */
 INSERT INTO m_user (user_id, encrypted_password, user_name, user_permission, gender, age, birth_date, address, phone_number)
-VALUES('kumamoto@hcs.ac.jp', '$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2ASFjDhe6WF4.Qji1huIPa', '隈元悠翔', 'ADMIN', 1, 21, '2000-07-31', '札幌市東区北30条西2丁目9-12', '090-8254-9913');
+VALUES('kumamoto@admin.ac.jp', '$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2ASFjDhe6WF4.Qji1huIPa', '隈元悠翔', 'ADMIN', 1, 21, '2000-07-31', '札幌市東区北30条西2丁目9-12', '090-8254-9913');
 /* ユーザマスタのデータ（一般権限） PASS:pasword */
 INSERT INTO m_user (user_id, encrypted_password, user_name, user_permission, gender, age, birth_date, address, phone_number)
 VALUES('kashiwaya@hcs.ac.jp', '$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2ASFjDhe6WF4.Qji1huIPa', '柏谷弘輝', 'GENERAL', 1, 21, '2000-08-16', '札幌市北区南1条東6丁目11-4', '090-5204-0942');
